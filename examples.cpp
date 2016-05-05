8fcb7c3eaaf7f4f73fe4deb286d2432db4500d32
U-olece-PC\olece <eidheim@gmail.com>
2016-05-05 18:16:05 +0200

    Fixes examples for Windows without MSYS2. Related to #11

--- examples.cpp
+++ examples.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main() {
+#if !defined(_WIN32) || defined(MSYS_PROCESS_USE_SH)
+  //The following examples are for Unix-like systems and Windows through MSYS2
+
+
   cout << "Example 1 - the mandatory Hello World" << endl;
   Process process1("echo Hello World", "", [](const char *bytes, size_t n) {
     cout << "Output from stdout: " << std::string(bytes, n);
@@ -47,11 +51,8 @@ int main() {
   std::this_thread::sleep_for(std::chrono::seconds(5));
   process4->kill();
   std::this_thread::sleep_for(std::chrono::seconds(5));
-  
-  
-#if !defined(_WIN32) || defined(MSYS_PROCESS_USE_SH)
-  //Example 5 only works when having access to the sh command (for multiple commands)
-  //On Windows: install MSYS2 (https://msys2.github.io)
+
+
   cout << endl << "Example 5 - multiple commands, stdout and stderr" << endl;
   Process process5("echo Hello && ls an_incorrect_path", "", [](const char *bytes, size_t n) {
     cout << "Output from stdout: " << std::string(bytes, n);
@@ -66,8 +67,6 @@ int main() {
   std::this_thread::sleep_for(std::chrono::seconds(5));
   
   
-  //Example 6 only works when having access to the bash command
-  //On Windows: install MSYS2 (https://msys2.github.io)
   cout << endl << "Example 6 - run bash with input from stdin" << endl;
   Process process6("bash", "", [](const char *bytes, size_t n) {
     cout << "Output from stdout: " << std::string(bytes, n);
@@ -79,8 +78,6 @@ int main() {
   std::this_thread::sleep_for(std::chrono::seconds(5));
   
   
-  //Example 7 only works when having access to the cat command
-  //On Windows: install MSYS2 (https://msys2.github.io)
   cout << endl << "Example 7 - send data to cat through stdin" << endl;
   Process process7("cat", "", [](const char *bytes, size_t n) {
     cout << "Output from stdout: " << std::string(bytes, n);
@@ -90,6 +87,58 @@ int main() {
   exit_status=process7.get_exit_status();
   cout << "Example 7 process returned: " << exit_status << " (" << (exit_status==0?"success":"failure") << ")" << endl;
   std::this_thread::sleep_for(std::chrono::seconds(5));
+
+
+#else
+  //Examples for Windows without MSYS2
+
+
+  cout << "Example 1 - the mandatory Hello World" << endl;
+  Process process1("cmd /C echo Hello World", "", [](const char *bytes, size_t n) {
+    cout << "Output from stdout: " << std::string(bytes, n);
+  });
+  auto exit_status=process1.get_exit_status();
+  cout << "Example 1 process returned: " << exit_status << " (" << (exit_status==0?"success":"failure") << ")" << endl;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  
+  
+  cout << endl << "Example 2 - cd into a nonexistent directory" << endl;
+  Process process2("cmd /C cd a_nonexistent_directory", "", [](const char *bytes, size_t n) {
+    cout << "Output from stdout: " << std::string(bytes, n);
+  }, [](const char *bytes, size_t n) {
+    cout << "Output from stderr: " << std::string(bytes, n);
+    //add a newline for prettier output on some platforms:
+    if(bytes[n-1]!='\n')
+      cout << endl;
+  });
+  exit_status=process2.get_exit_status();
+  cout << "Example 2 process returned: " << exit_status << " (" << (exit_status==0?"success":"failure") << ")" << endl;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  
+  
+  cout << endl << "Example 3 - async sleep process" << endl;
+  std::thread thread3([]() {
+    Process process3("timeout 5");
+    auto exit_status=process3.get_exit_status();
+    cout << "Example 3 process returned: " << exit_status << " (" << (exit_status==0?"success":"failure") << ")" << endl;
+  });
+  thread3.detach();
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  
+  
+  cout << endl << "Example 4 - killing async sleep process after 5 seconds" << endl;
+  auto process4=std::make_shared<Process>("timeout 10");
+  std::thread thread4([process4]() {
+    auto exit_status=process4->get_exit_status();
+    cout << "Example 4 process returned: " << exit_status << " (" << (exit_status==0?"success":"failure") << ")" << endl;
+  });
+  thread4.detach();
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  process4->kill();
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
+
 #endif
+
   return 0;
 }
